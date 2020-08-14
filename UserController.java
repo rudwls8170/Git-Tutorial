2c9994306a75a3f15753e41bae02c5647bdedf7c
rudwls8170 <rudwls0201@naver.com>
2020-08-14 18:31:16 +0900

    Add UserController [update]

--- UserController.java
+++ UserController.java
@@ -14,4 +14,11 @@ public class UserController{
 		logger.info("경로 : insert");
 		return "user/insert";
 	}
+
+	// 회원수정 페이지로 이동
+	@RequestMapping("/user/update.do")
+	public String update(){
+		logger.info("경로 : update");
+		return "user/update";
+	}
 }
